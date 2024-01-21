f5227112c758e780db8ee13e5e5b88e6bcfc207c
popomon <popomonclub@gmail.com>
2024-01-21 23:16:10 +0900

    ci: Added html template file and set html template webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: 'Output Management',
+			template: './src/index.html',
 		}),
 		new MiniCssExtractPlugin({filename: 'style.css'}),
 	],
